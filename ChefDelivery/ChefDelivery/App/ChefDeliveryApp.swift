4821ce5c960fbdca180ebe50160c4b0c55b09876
Kleiton Mendes <kleitonmendes@MacBook-Air-de-Kleiton.local>
2025-08-22 16:51:25 -0300

    feat: create a NavigationBar

---
+++ ChefDelivery/ChefDelivery/App/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Kleiton Mendes on 21/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
