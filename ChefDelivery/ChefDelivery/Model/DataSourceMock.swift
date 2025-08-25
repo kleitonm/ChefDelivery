499dd0dff9c2b0620a3f9a6c9fc41a94900afd55
Kleiton Mendes <kleitonmendes@MacBook-Air-de-Kleiton.local>
2025-08-25 19:18:49 -0300

    create a gridItem

---
+++ ChefDelivery/ChefDelivery/Model/DataSourceMock.swift
@@ -0,0 +1,16 @@
+//
+//  DataSourceMock.swift
+//  ChefDelivery
+//
+//  Created by Kleiton Mendes on 25/08/25.
+//
+
+let ordersMock: [OrderType] = [
+    OrderType(id: 1, name: "Restaurantes", image: "hamburguer"),
+    OrderType(id: 2, name: "Mercado", image: "mercado"),
+    OrderType(id: 3, name: "Farmácia", image: "farmacia"),
+    OrderType(id: 4, name: "Pet", image: "petshop"),
+    OrderType(id: 5, name: "Desconto", image: "descontos"),
+    OrderType(id: 6, name: "Bebidas", image: "bebidas"),
+    OrderType(id: 7, name: "Gourmet", image: "gourmet")
+]
