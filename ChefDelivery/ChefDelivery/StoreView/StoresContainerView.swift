684ba92566bdd0446b9bf5b18d9584632a00e6b0
Kleiton Mendes <kleitonmendes@MacBook-Air-de-Kleiton.local>
2025-08-26 19:10:03 -0300

    create a StoreList

---
+++ ChefDelivery/ChefDelivery/StoreView/StoresContainerView.swift
@@ -0,0 +1,34 @@
+//
+//  StoresContainerView.swift
+//  ChefDelivery
+//
+//  Created by Kleiton Mendes on 26/08/25.
+//
+
+import SwiftUI
+
+struct StoresContainerView: View {
+    
+    let title = "Lojas"
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.headline)
+            
+            VStack(alignment: .leading, spacing: 30) {
+                ForEach(storesMock) { mock in
+                    StoreItemView(order: mock)
+                }
+            }
+        }
+        .padding(20)
+    }
+}
+
+struct StoresContainerView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoresContainerView()
+        .previewLayout(.sizeThatFits)
+    }
+}
