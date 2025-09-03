4aec4781ae4ddac5b2acc990f41284121a382c1c
Kleiton Mendes <kleitonmendes@MacBook-Air-de-Kleiton.local>
2025-09-03 11:52:55 -0300

    feat: create  filter

--- ChefDelivery/ChefDelivery/StoreView/StoresContainerView.swift
+++ ChefDelivery/ChefDelivery/StoreView/StoresContainerView.swift
@@ -10,18 +10,54 @@ import SwiftUI
 struct StoresContainerView: View {
     
     let title = "Lojas"
+    @State private var ratingFilter = 0
+    var filteredStores: [StoreType] {
+        return storesMock.filter { store in
+            store.stars >= ratingFilter
+        }
+    }
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text(title)
-                .font(.headline)
+            HStack {
+                Text(title)
+                    .font(.headline)
+                
+                Spacer()
+                
+                Menu("Filtrar") {
+                    
+                    ForEach(1...5, id: \.self) { rating in
+                        Button {
+                            ratingFilter = rating
+                        } label: {
+                            if rating > 1 {
+                                Text("\(rating) estrelas ou mais")
+                            } else {
+                                Text("\(rating) estrela ou mais")
+                            }
+                        }
+                    }
+                }
+                .foregroundColor(.black)
+            }
             
             VStack(alignment: .leading, spacing: 30) {
-                ForEach(storesMock) { mock in
-                    NavigationLink {
-                        StoreDetailView(store: mock) 
-                    } label: {
-                        StoreItemView(store: mock)
+                
+                if filteredStores.isEmpty {
+                    Text("Nenhum resultado encontrado.")
+                        .bold()
+                        .foregroundColor(Color("ColorRed"))
+                        .font(.title2)
+                        .padding(.vertical, 32)
+                        .frame(maxWidth: .infinity)
+                } else {
+                    ForEach(filteredStores) { mock in
+                        NavigationLink {
+                            StoreDetailView(store: mock)
+                        } label: {
+                            StoreItemView(store: mock)
+                        }
                     }
                 }
             }
