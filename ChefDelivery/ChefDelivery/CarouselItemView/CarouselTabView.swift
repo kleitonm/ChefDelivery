41604fb148f697315b78920158ef2fa9a5fd70ab
Kleiton Mendes <kleitonmendes@MacBook-Air-de-Kleiton.local>
2025-09-08 19:04:28 -0300

    input animation in Carousel

--- ChefDelivery/ChefDelivery/CarouselItemView/CarouselTabView.swift
+++ ChefDelivery/ChefDelivery/CarouselItemView/CarouselTabView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CarouselTabView: View {
     
+    @State private var currentIndex: Int = 0
+    
     let ordersMock: [OrderType] = [
         OrderType(id: 1, name: "barbecue-banner", image: "barbecue-banner"),
         OrderType(id: 2, name: "brazilian-meal-banner", image: "brazilian-meal-banner"),
@@ -16,13 +18,24 @@ struct CarouselTabView: View {
     ]
     
     var body: some View {
-        TabView {
+        TabView(selection: $currentIndex) {
             ForEach(ordersMock) { mock in
                 CarouselItemView(order: mock)
+                    .tag(mock.id)
             }
         }
         .frame(height: 180)
         .tabViewStyle(.page(indexDisplayMode: .always))
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { _ in
+                withAnimation(.easeInOut(duration: 1)) {
+                    if currentIndex > ordersMock.count {
+                        currentIndex = 1
+                    }
+                    currentIndex += 1
+                }
+            }
+        }
     }
 }
 
